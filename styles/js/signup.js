95804c01f66b571c2dfa3e31ff7bbbf77f3263ee
Arman Issayev <144832613+armanissayev@users.noreply.github.com>
2024-11-12 00:43:16 +0500

    added EmailCheck API

--- styles/js/signup.js
+++ styles/js/signup.js
@@ -1,13 +1,40 @@
-document.getElementById("signupForm").addEventListener("submit", function(event) {
+async function checkEmail(email) {
+  try {
+    const response = await fetch(`https://api.emailvalidation.io/v1/info?apikey=ema_live_vIWmWjgeIXvlvqulTyw1Z3L5o1wIEiH2NiL79R8l&email=${email}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    const data = await response.json();
+
+    console.log(data);
+
+    if (!data.smtp_check) return false;
+    else return true;
+
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+    return false;
+  }
+}
+
+document.getElementById("signupForm").addEventListener("submit", async function(event) {
     event.preventDefault(); 
     
-    let username = document.getElementById("username").value;
+    let firstName = document.getElementById("firstName").value;
+    let secondName = document.getElementById("lastName").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirmPassword").value;
     let errorMessage = document.getElementById("error-message");
 
-    if (username === "" || email === "" || password === "" || confirmPassword === "") {
+    let res = await checkEmail(email);
+
+    if (!res) {
+        alert("Email is not correct!");
+        return;
+    }
+
+    if (firstName === "" || email === "" || password === "" || confirmPassword === "") {
         errorMessage.textContent = "All fields are required.";
     } else if (password.length < 8) {
         errorMessage.textContent = "Password must be at least 8 characters long.";
@@ -15,7 +42,7 @@ document.getElementById("signupForm").addEventListener("submit", function(event)
         errorMessage.textContent = "Passwords do not match.";
     } else {
         errorMessage.textContent = ""; 
-        localStorage.setItem("username", username);
+        localStorage.setItem("firstName", firstName);
         localStorage.setItem("password", password);
         alert("Account created successfully!");
         window.location.href = "login.html";
