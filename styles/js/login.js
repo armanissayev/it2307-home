04729410669859d261cc4d8c18c4e9dcc80ad7fd
Arman Issayev <144832613+armanissayev@users.noreply.github.com>
2024-11-12 11:18:39 +0500

    Update login.js

--- styles/js/login.js
+++ styles/js/login.js
@@ -1,19 +1,19 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
-    let username = document.getElementById("username").value;
+    let firstName = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     let errorMessage = document.getElementById("error-message");
 
     // Retrieve stored credentials from localStorage
-    let storedUsername = localStorage.getItem("username");
+    let storedUsername = localStorage.getItem("firstName");
     let storedPassword = localStorage.getItem("password");
 
-    if (username === "" || password === "") {
+    if (firstName === "" || password === "") {
         errorMessage.textContent = "Both fields are required.";
     } else if (password.length < 8) {
         errorMessage.textContent = "Password must be at least 8 characters long.";
-    } else if (username === storedUsername && password === storedPassword) {
+    } else if (firstName === storedUsername && password === storedPassword) {
         errorMessage.textContent = ""; 
         alert("Logged in successfully!");
         window.location.href = "index.html";  
